2dad9ddf253ac81f75bc6b9aa9a7c918af155d1a
DavidAdewale <adewaledavidola@gmail.com>
2025-05-03 07:47:29 +0100

    removed maries works from hero section

--- src/data/slideshowImages.js
+++ src/data/slideshowImages.js
@@ -1,11 +1,9 @@
 export const slideshowImages = [
   '/images/slideshow/in-retrospect.JPG',
   '/images/slideshow/living-bird.jpg',
-  '/images/slideshow/misery-loves-company.jpg',
   '/images/slideshow/oduduwas-descent.JPG',
   '/images/slideshow/scar.JPG',
   '/images/slideshow/take-care-of-me.jpg',
-  '/images/slideshow/when-is-a-door-not-a-door-ajar.jpg',
   '/images/slideshow/when-tomorrow-is-too-late.JPG',
   '/images/slideshow/white-noise.JPG',
 ];
